eecba151ba7f67e729efcec422cd652b162537a8
Yusuf Sabbag <sist.heb@gmail.com>
2021-03-11 02:23:50 +0300

    Added licence information

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+// Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License. 
+
 // enabilg popovers
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
