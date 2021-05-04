fb36c1d74027a1cb9cc7f59d4b5d44030b13c88b
Yusuf Sabbag <sist.heb@gmail.com>
2021-05-04 21:16:15 +0300

    popover
    Replaced the poppoverJS with my own with pure js in index.html

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-// Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License. 
-
-// enabilg popovers
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
---
+++ JS/script_index.js
@@ -0,0 +1,15 @@
+// Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License.
+
+// =============================== //
+// the footer language link
+const lgLink = document.querySelector(".language"); // the link
+const lgInfo = document.querySelector(".language-description"); // the link's info
+
+lgLink.addEventListener("click", () => {
+    if (lgInfo.classList.contains("lginfo-visibility")) {
+        lgInfo.classList.remove("lginfo-visibility");
+        setTimeout(function() {
+            lgInfo.classList.add("lginfo-visibility");
+        }, 1500)
+    }
+});
