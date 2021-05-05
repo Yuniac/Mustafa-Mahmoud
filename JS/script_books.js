0991149b41f063b36b9e084988ab55e24f0096dc
Yusuf Sabbag <sist.heb@gmail.com>
2021-05-05 17:26:50 +0300

    Added the footer popver

--- JS/script_books.js
+++ JS/script_books.js
@@ -49,3 +49,16 @@ missingLinks.forEach(link => {
         }
     })
 })
+
+// the footer language popver 
+const lgLink = document.querySelector(".language"); // the link
+const lgInfo = document.querySelector(".language-description"); // the link's info
+
+lgLink.addEventListener("click", () => {
+    if (lgInfo.classList.contains("lginfo-visibility")) {
+        lgInfo.classList.remove("lginfo-visibility");
+        setTimeout(function() {
+            lgInfo.classList.add("lginfo-visibility");
+        }, 1500)
+    }
+});
