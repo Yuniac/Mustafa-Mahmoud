ff68c4c8e82cb536e17791576a032e967ccdb701
Yusuf Sabbagh <sist.heb@gmail.com>
2021-05-12 23:05:37 +0300

    Update script_lifestory.js

--- JS/script_lifestory.js
+++ JS/script_lifestory.js
@@ -31,7 +31,7 @@ function changeImgF() {
     carouselImg.style.opacity = 0;
     setTimeout(() => {
         carouselImg.style.opacity = 1;
-    }, 650);
+    }, 750);
 }
 
 function changeImgB() {
@@ -47,7 +47,7 @@ function changeImgB() {
     carouselImg.style.opacity = 0;
     setTimeout(() => {
         carouselImg.style.opacity = 1;
-    }, 650);
+    }, 750);
 }
 forwardImg.addEventListener("click", changeImgF);
 backwardImg.addEventListener("click", changeImgB);
