6fa3f07be17e08a7fa4d8d3fe85fcd3a6fe034ab
Yusuf Sabbag <sist.heb@gmail.com>
2021-10-25 03:45:07 +0300

    removed an unneeded feature

--- JS/script_index.js
+++ JS/script_index.js
@@ -14,27 +14,26 @@ const interview5 = document.querySelector(".interview-body-5");
 const expandAccordion6 = document.querySelector("#expandIcon6");
 const interview6 = document.querySelector(".interview-body-6");
 
-
 // the acordion function
 const accordion = (interview, interviewButton) => {
-    interviewButton.addEventListener("click", () => {
-        if (interview.style.visibility === "visible") {
-            interview.style.visibility = "hidden";
-            interview.style.maxHeight = 0 + "px";
-            interviewButton.style.rotate = "0deg";
-            setTimeout(() => {
-                interview.style.display = "none"
-            }, 900)
-        } else {
-            interview.style.display = "block";
-            setTimeout(() => {
-                interview.style.visibility = "visible"
-                interview.style.maxHeight = 400 + "px";
-                interviewButton.style.rotate = "180deg";
-            }, 100)
-        }
-    })
-}
+	interviewButton.addEventListener("click", () => {
+		if (interview.style.visibility === "visible") {
+			interview.style.visibility = "hidden";
+			interview.style.maxHeight = 0 + "px";
+			interviewButton.style.rotate = "0deg";
+			setTimeout(() => {
+				interview.style.display = "none";
+			}, 900);
+		} else {
+			interview.style.display = "block";
+			setTimeout(() => {
+				interview.style.visibility = "visible";
+				interview.style.maxHeight = 400 + "px";
+				interviewButton.style.rotate = "180deg";
+			}, 100);
+		}
+	});
+};
 
 accordion(interview1, expandAccordion1);
 accordion(interview2, expandAccordion2);
@@ -43,57 +42,54 @@ accordion(interview4, expandAccordion4);
 accordion(interview5, expandAccordion5);
 accordion(interview6, expandAccordion6);
 
-
 // the navbar
 const menuButton = document.querySelector(".current-page");
 const navBar = document.querySelector(".sub-nav-items");
 menuButton.addEventListener("click", () => {
-    if (navBar.style.height === "0px") {
-        navBar.style.height = "220px";
-    } else {
-        navBar.style.height = "0px";
-    }
+	if (navBar.style.height === "0px") {
+		navBar.style.height = "220px";
+	} else {
+		navBar.style.height = "0px";
+	}
 });
-
 
 // the footer links
 const lgLink = document.querySelector(".language"); // the language link
 const lgInfo = document.querySelector(".language-description"); // the link's info
 
 lgLink.addEventListener("click", () => {
-    if (lgInfo.classList.contains("lginfo-visibility")) {
-        lgInfo.classList.remove("lginfo-visibility");
-        setTimeout(function() {
-            lgInfo.classList.add("lginfo-visibility");
-        }, 1500)
-    }
+	if (lgInfo.classList.contains("lginfo-visibility")) {
+		lgInfo.classList.remove("lginfo-visibility");
+		setTimeout(function () {
+			lgInfo.classList.add("lginfo-visibility");
+		}, 1500);
+	}
 });
 
-const copyLink = document.querySelector(".special-li");
-const copyInfo = document.querySelector(".copy-notice");
-const notCopied = "أنقر لنسخ رابط الموقع";
-const copied = "تم النسخ!";
-copyInfo.textContent = notCopied;
+// const copyLink = document.querySelector(".special-li");
+// const copyInfo = document.querySelector(".copy-notice");
+// const notCopied = "أنقر لنسخ رابط الموقع";
+// const copied = "تم النسخ!";
+// copyInfo.textContent = notCopied;
 
-copyLink.addEventListener("mouseover", () => {
-    //some delay to make it smooth
-    setTimeout(() => {
-        if (copyInfo.classList.contains("copy-notice-visibility")) {
-            copyInfo.classList.remove("copy-notice-visibility");
-            setTimeout(function() {
-                copyInfo.classList.add("copy-notice-visibility");
-            }, 1500)
-        }
-    }, 400)
-});
+// copyLink.addEventListener("mouseover", () => {
+//     //some delay to make it smooth
+//     setTimeout(() => {
+//         if (copyInfo.classList.contains("copy-notice-visibility")) {
+//             copyInfo.classList.remove("copy-notice-visibility");
+//             setTimeout(function() {
+//                 copyInfo.classList.add("copy-notice-visibility");
+//             }, 1500)
+//         }
+//     }, 400)
+// });
 
-
-copyLink.addEventListener("click", () => {
-    let websiteLink = document.querySelector("#link");
-    let copiedWebsiteLink = websiteLink.textContent;
-    navigator.clipboard.writeText(copiedWebsiteLink);
-    copyInfo.textContent = copied;
-    setTimeout(() => {
-        copyInfo.textContent = notCopied;
-    }, 2000)
-})
+// copyLink.addEventListener("click", () => {
+//     let websiteLink = document.querySelector("#link");
+//     let copiedWebsiteLink = websiteLink.textContent;
+//     navigator.clipboard.writeText(copiedWebsiteLink);
+//     copyInfo.textContent = copied;
+//     setTimeout(() => {
+//         copyInfo.textContent = notCopied;
+//     }, 2000)
+// })
--- JS/script_lifestory.js
+++ JS/script_lifestory.js
@@ -1,11 +1,11 @@
-// Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License. 
+// Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License.
 
 // the notice div
 const noticeButton = document.querySelector("#hideButton");
 const noticeDiv = document.querySelector("#noticeDiv");
 noticeButton.addEventListener("click", () => {
-    noticeDiv.style.visibility = "hidden";
-    noticeDiv.style.display = "none";
+	noticeDiv.style.visibility = "hidden";
+	noticeDiv.style.display = "none";
 });
 
 // the carousel
@@ -20,61 +20,59 @@ const backwardImg = document.querySelector(".backward-icon");
 const forwardImg = document.querySelector(".forward-icon");
 
 function changeImgF() {
-    if (i === imgCount) {
-        i = 1;
-    } else if (i >= 1) {
-        i++
-    }
-    setTimeout(() => {
-        imgURL = `${tempURL}${i}${imgExtension}`;
-        carouselImg.src = imgURL;
-    }, 650)
-    carouselImg.style.opacity = 0;
-    setTimeout(() => {
-        carouselImg.style.opacity = 1;
-    }, 750);
+	if (i === imgCount) {
+		i = 1;
+	} else if (i >= 1) {
+		i++;
+	}
+	setTimeout(() => {
+		imgURL = `${tempURL}${i}${imgExtension}`;
+		carouselImg.src = imgURL;
+	}, 650);
+	carouselImg.style.opacity = 0;
+	setTimeout(() => {
+		carouselImg.style.opacity = 1;
+	}, 750);
 }
 
 function changeImgB() {
-    if (i === 1) {
-        i = imgCount + 1;
-        // extra 1 on the amount of all images because we have to decrease it by 1
-    };
-    i--;
-    setTimeout(() => {
-        imgURL = `${tempURL}${i}${imgExtension}`;
-        carouselImg.src = imgURL;
-    }, 650)
-    carouselImg.style.opacity = 0;
-    setTimeout(() => {
-        carouselImg.style.opacity = 1;
-    }, 750);
+	if (i === 1) {
+		i = imgCount + 1;
+		// extra 1 on the amount of all images because we have to decrease it by 1
+	}
+	i--;
+	setTimeout(() => {
+		imgURL = `${tempURL}${i}${imgExtension}`;
+		carouselImg.src = imgURL;
+	}, 650);
+	carouselImg.style.opacity = 0;
+	setTimeout(() => {
+		carouselImg.style.opacity = 1;
+	}, 750);
 }
 forwardImg.addEventListener("click", changeImgF);
 backwardImg.addEventListener("click", changeImgB);
 
-
-// scroll to top button 
+// scroll to top button
 let goTopButton = document.getElementById("scrollToTopButton");
 let root = document.documentElement;
 
-
 goTopButton.addEventListener("click", function goTop() {
-    root.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
+	root.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
 });
 
 // the navbar
 const menuButton = document.querySelector(".current-page");
 const navBar = document.querySelector(".sub-nav-items");
 menuButton.addEventListener("click", () => {
-    if (navBar.style.height === "0px") {
-        navBar.style.height = "220px";
-    } else {
-        navBar.style.height = "0px";
-    }
+	if (navBar.style.height === "0px") {
+		navBar.style.height = "220px";
+	} else {
+		navBar.style.height = "0px";
+	}
 });
 
 // the footer links
@@ -82,39 +80,38 @@ const lgLink = document.querySelector(".language"); // the language link
 const lgInfo = document.querySelector(".language-description"); // the link's info
 
 lgLink.addEventListener("click", () => {
-    if (lgInfo.classList.contains("lginfo-visibility")) {
-        lgInfo.classList.remove("lginfo-visibility");
-        setTimeout(function() {
-            lgInfo.classList.add("lginfo-visibility");
-        }, 1500)
-    }
+	if (lgInfo.classList.contains("lginfo-visibility")) {
+		lgInfo.classList.remove("lginfo-visibility");
+		setTimeout(function () {
+			lgInfo.classList.add("lginfo-visibility");
+		}, 1500);
+	}
 });
 
-const copyLink = document.querySelector(".special-li");
-const copyInfo = document.querySelector(".copy-notice");
-const notCopied = "أنقر لنسخ رابط الموقع";
-const copied = "تم النسخ!";
-copyInfo.textContent = notCopied;
+// const copyLink = document.querySelector(".special-li");
+// const copyInfo = document.querySelector(".copy-notice");
+// const notCopied = "أنقر لنسخ رابط الموقع";
+// const copied = "تم النسخ!";
+// copyInfo.textContent = notCopied;
 
-copyLink.addEventListener("mouseover", () => {
-    //some delay to make it smooth
-    setTimeout(() => {
-        if (copyInfo.classList.contains("copy-notice-visibility")) {
-            copyInfo.classList.remove("copy-notice-visibility");
-            setTimeout(function() {
-                copyInfo.classList.add("copy-notice-visibility");
-            }, 1500)
-        }
-    }, 400)
-});
+// copyLink.addEventListener("mouseover", () => {
+//     //some delay to make it smooth
+//     setTimeout(() => {
+//         if (copyInfo.classList.contains("copy-notice-visibility")) {
+//             copyInfo.classList.remove("copy-notice-visibility");
+//             setTimeout(function() {
+//                 copyInfo.classList.add("copy-notice-visibility");
+//             }, 1500)
+//         }
+//     }, 400)
+// });
 
-
-copyLink.addEventListener("click", () => {
-    let websiteLink = document.querySelector("#link");
-    let copiedWebsiteLink = websiteLink.textContent;
-    navigator.clipboard.writeText(copiedWebsiteLink);
-    copyInfo.textContent = copied;
-    setTimeout(() => {
-        copyInfo.textContent = notCopied;
-    }, 2000)
-})
+// copyLink.addEventListener("click", () => {
+//     let websiteLink = document.querySelector("#link");
+//     let copiedWebsiteLink = websiteLink.textContent;
+//     navigator.clipboard.writeText(copiedWebsiteLink);
+//     copyInfo.textContent = copied;
+//     setTimeout(() => {
+//         copyInfo.textContent = notCopied;
+//     }, 2000)
+// })
