dcec8b6eb6b6d66fc413e6ec42cc135fd4a01f22
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-19 23:58:36 +0300

    added cors middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const { router } = require("./router/api");
+const { allowCrossDomain } = require("./helpers/helpers");
 
+app.use(allowCrossDomain);
 app.use("/", router);
 
 app.listen(5000, () => console.log("app running at port 5000..."));
