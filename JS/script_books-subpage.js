ee788398e7fccb42db6b3f5416561443fa39326a
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-19 23:57:17 +0300

    loadBooks function to fetch books from db

--- JS/script_books-subpage.js
+++ JS/script_books-subpage.js
@@ -1,5 +1,82 @@
 // Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License.
 
+const loadBooks = async () => {
+	const categoriesForBooksPage = [121, 19, 20, 77, 36, 84];
+	const gallerySection = document.querySelector("section.gallery");
+	for (let i = 0; i < categoriesForBooksPage.length; i++) {
+		const books = await (await fetch(`http://localhost:5000/books?category=${categoriesForBooksPage[i]}`)).json();
+		// each category has a wrapper
+		const categoryContainer = document.createElement("div");
+		categoryContainer.classList.add("category-container");
+
+		const booksWrapper = document.createElement("div");
+		booksWrapper.classList.add("books-wrapper");
+
+		gallerySection.append(categoryContainer);
+
+		const categoryHeader = document.createElement("h2");
+		categoryHeader.classList.add("category-header");
+		// the category name
+		categoryHeader.textContent = books.data[0].subject;
+		categoryContainer.append(categoryHeader);
+		books.data.forEach((book) => {
+			const bookCardDiv = document.createElement("div");
+			bookCardDiv.classList.add("book-card");
+
+			//
+			const bookUpper = document.createElement("div");
+			bookUpper.classList.add("book-upper");
+
+			const bookLower = document.createElement("div");
+			bookLower.classList.add("book-lower", "book-info");
+
+			//
+			const bookImg = document.createElement("img");
+			bookImg.src = book.links.img;
+
+			const bookTitle = document.createElement("p");
+			bookTitle.textContent = book.name;
+
+			bookUpper.append(bookImg);
+			bookUpper.append(bookTitle);
+
+			//
+			const bookBio = document.createElement("div");
+			bookLower.classList.add("book-bio");
+
+			const bookSubject = document.createElement("p");
+			bookSubject.textContent = book.subject;
+
+			const bookPublishingYear = document.createElement("p");
+			bookPublishingYear.textContent = book.publishing_year;
+
+			bookBio.append(bookSubject);
+			bookBio.append(bookPublishingYear);
+
+			const bookLinks = document.createElement("div");
+			bookLower.classList.add("book-links");
+
+			const wikiLink = document.createElement("a");
+			if (book.links.wiki_link === "") {
+				wikiLink.classList.add("missing-wiki-source");
+			} else {
+				wikiLink.href = book.links.wiki_link;
+			}
+			wikiLink.textContent = "ويكيبيديا";
+
+			bookLinks.append(wikiLink);
+
+			bookLower.append(bookBio);
+			bookLower.append(bookLinks);
+
+			bookCardDiv.append(bookUpper);
+			bookCardDiv.append(bookLower);
+			booksWrapper.append(bookCardDiv);
+		});
+		categoryContainer.append(booksWrapper);
+	}
+};
+loadBooks();
 // the popovers for the missing links of the books
 const wikiLinks = document.querySelectorAll(".missing-wiki-source");
 wikiLinks.forEach((link) => {
--- JS/script_books.js
+++ JS/script_books.js
@@ -1,5 +1,83 @@
 // Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License.
 
+const loadBooks = async () => {
+	const categoriesForBooksPage = [97, 52, 3, 1, 67];
+	const gallerySection = document.querySelector("section.gallery");
+	for (let i = 0; i < categoriesForBooksPage.length; i++) {
+		const books = await (await fetch(`http://localhost:5000/books?category=${categoriesForBooksPage[i]}`)).json();
+		// each category has a wrapper
+		const categoryContainer = document.createElement("div");
+		categoryContainer.classList.add("category-container");
+
+		const booksWrapper = document.createElement("div");
+		booksWrapper.classList.add("books-wrapper");
+
+		gallerySection.append(categoryContainer);
+
+		const categoryHeader = document.createElement("h2");
+		categoryHeader.classList.add("category-header");
+		// the category name
+		categoryHeader.textContent = books.data[0].subject;
+		categoryContainer.append(categoryHeader);
+		books.data.forEach((book) => {
+			const bookCardDiv = document.createElement("div");
+			bookCardDiv.classList.add("book-card");
+
+			//
+			const bookUpper = document.createElement("div");
+			bookUpper.classList.add("book-upper");
+
+			const bookLower = document.createElement("div");
+			bookLower.classList.add("book-lower", "book-info");
+
+			//
+			const bookImg = document.createElement("img");
+			bookImg.src = book.links.img;
+
+			const bookTitle = document.createElement("p");
+			bookTitle.textContent = book.name;
+
+			bookUpper.append(bookImg);
+			bookUpper.append(bookTitle);
+
+			//
+			const bookBio = document.createElement("div");
+			bookBio.classList.add("book-bio");
+
+			const bookSubject = document.createElement("p");
+			bookSubject.textContent = book.subject;
+
+			const bookPublishingYear = document.createElement("p");
+			bookPublishingYear.textContent = book.publishing_year;
+
+			bookBio.append(bookSubject);
+			bookBio.append(bookPublishingYear);
+
+			const bookLinks = document.createElement("div");
+			bookLinks.classList.add("book-links");
+
+			const wikiLink = document.createElement("a");
+			if (book.links.wiki_link === "") {
+				wikiLink.classList.add("missing-wiki-source");
+			} else {
+				wikiLink.href = book.links.wiki_link;
+			}
+			wikiLink.textContent = "ويكيبيديا";
+
+			bookLinks.append(wikiLink);
+
+			bookLower.append(bookBio);
+			bookLower.append(bookLinks);
+
+			bookCardDiv.append(bookUpper);
+			bookCardDiv.append(bookLower);
+			booksWrapper.append(bookCardDiv);
+		});
+		categoryContainer.append(booksWrapper);
+	}
+};
+// TODO loading animation
+loadBooks();
 // the popovers for the missing links of the books
 const wikiLinks = document.querySelectorAll(".missing-wiki-source");
 
@@ -22,18 +100,21 @@ wikiLinks.forEach((link) => {
 
 // highlight function
 // TODO change base url
+
 window.addEventListener("load", () => {
 	// const baseUrl = "https://www.mustafamahmoud.net/books.html";
 	const baseUrl = "file:///D:/G%20Drive/All%20Info/web%20development/WD/Mustafa-Mahmoud/books.html";
 	const hash = window.location.hash;
-	const el = document.querySelector(hash);
-	const elMatchingUrl = `${baseUrl}${hash}`;
+	if (hash) {
+		const el = document.querySelector(hash);
+		const elMatchingUrl = `${baseUrl}${hash}`;
 
-	if (window.location.href === elMatchingUrl) {
-		el.classList.add("highlight");
-		setTimeout(() => {
-			el.classList.remove("highlight");
-		}, 2500);
+		if (window.location.href === elMatchingUrl) {
+			el.classList.add("highlight");
+			setTimeout(() => {
+				el.classList.remove("highlight");
+			}, 2500);
+		}
 	}
 });
 // the navbar
