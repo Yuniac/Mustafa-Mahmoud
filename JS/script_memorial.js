74acd3a5f2d4c5ff3c5ec619c0543d8ce5537e4c
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-19 23:57:31 +0300

    removed comments

--- JS/script_memorial.js
+++ JS/script_memorial.js
@@ -23,31 +23,3 @@ lgLink.addEventListener("click", () => {
 		}, 1500);
 	}
 });
-
-// const copyLink = document.querySelector(".special-li");
-// const copyInfo = document.querySelector(".copy-notice");
-// const notCopied = "أنقر لنسخ رابط الموقع";
-// const copied = "تم النسخ!";
-// copyInfo.textContent = notCopied;
-
-// copyLink.addEventListener("mouseover", () => {
-//     //some delay to make it smooth
-//     setTimeout(() => {
-//         if (copyInfo.classList.contains("copy-notice-visibility")) {
-//             copyInfo.classList.remove("copy-notice-visibility");
-//             setTimeout(function() {
-//                 copyInfo.classList.add("copy-notice-visibility");
-//             }, 1500)
-//         }
-//     }, 400)
-// });
-
-// copyLink.addEventListener("click", () => {
-//     let websiteLink = document.querySelector("#link");
-//     let copiedWebsiteLink = websiteLink.textContent;
-//     navigator.clipboard.writeText(copiedWebsiteLink);
-//     copyInfo.textContent = copied;
-//     setTimeout(() => {
-//         copyInfo.textContent = notCopied;
-//     }, 2000)
-// })
