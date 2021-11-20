1809231b50ec9d22b02365c7676a59577f93fc49
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-20 22:54:02 +0300

    updated base route

--- server/router/website.js
+++ server/router/website.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/", (req, res) => {
+router.get("/index", (req, res) => {
 	res.render("main_pages/index");
 });
 
