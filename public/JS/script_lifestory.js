a976a6846487a4520385ddcd3eec822f1bc38dd1
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-21 22:08:04 +0300

    removed unneeded code

--- public/JS/script_lifestory.js
+++ public/JS/script_lifestory.js
@@ -64,17 +64,6 @@ goTopButton.addEventListener("click", function goTop() {
 	});
 });
 
-// the navbar
-const menuButton = document.querySelector(".current-page");
-const navBar = document.querySelector(".sub-nav-items");
-menuButton.addEventListener("click", () => {
-	if (navBar.style.height === "0px") {
-		navBar.style.height = "220px";
-	} else {
-		navBar.style.height = "0px";
-	}
-});
-
 // the footer links
 const lgLink = document.querySelector(".language"); // the language link
 const lgInfo = document.querySelector(".language-description"); // the link's info
--- public/JS/script_memorial.js
+++ public/JS/script_memorial.js
@@ -1,16 +1,5 @@
 // Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License.
 
-// the navbar
-const menuButton = document.querySelector(".current-page");
-const navBar = document.querySelector(".sub-nav-items");
-menuButton.addEventListener("click", () => {
-	if (navBar.style.height === "0px") {
-		navBar.style.height = "220px";
-	} else {
-		navBar.style.height = "0px";
-	}
-});
-
 // the footer links
 const lgLink = document.querySelector(".language"); // the language link
 const lgInfo = document.querySelector(".language-description"); // the link's info
