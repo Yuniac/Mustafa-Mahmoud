ea61a349525fd5c965e409a7e206567d12ffa340
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-21 21:31:26 +0300

    highlight active tab programmatically

--- public/JS/website_helpers.js
+++ public/JS/website_helpers.js
@@ -1,4 +1,13 @@
 const highlightActivePage = () => {
-	console.log(window.location.href);
+	const navLinks = document.querySelectorAll(".nav-item");
+	for (let i = 0; i < navLinks.length; i++) {
+		if (window.location.pathname === navLinks[i].dataset.route) {
+			navLinks[i].classList.add("active");
+		}
+	}
+	if (window.location.pathname === "/literature-subpage") {
+		const literatureNav = document.querySelector("[data-route='/literature']");
+		literatureNav.classList.add("active");
+	}
 };
 highlightActivePage();
