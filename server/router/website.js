dc996311b0b588045912148d475437a20733a70d
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-22 15:30:45 +0300

    created route for /book/:id

--- server/router/website.js
+++ server/router/website.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+const { getBookById } = require("../controllers/get");
 
 router.get("/", (req, res) => {
 	res.render("main_pages/index");
@@ -17,8 +17,25 @@ router.get("/literature-subpage", (req, res) => {
 router.get("/lifestory", (req, res) => {
 	res.render("main_pages/lifestory");
 });
+
 router.get("/memorial", (req, res) => {
 	res.render("main_pages/memorial");
+});
+
+router.get("/book/:id", async (req, res) => {
+	const { id } = req.params;
+	const { name, publisher, publishing_year, subject, details, quotes, index, links } = await getBookById(id);
+	res.render("main_pages/book", {
+		name,
+		publisher,
+		publishing_year,
+		subject,
+		pages_count: details.pages_count,
+		copy_type: details.copy_type,
+		quotes,
+		chapters: index,
+		img: links.img,
+	});
 });
 
 module.exports = { router };
