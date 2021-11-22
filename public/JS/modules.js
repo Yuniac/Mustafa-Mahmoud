870fa8118ab73f2173f92de0d502899ec26658a1
Yusuf Sabbag <sist.heb@gmail.com>
2021-11-22 22:33:38 +0300

    shared functions are now served as a module

---
+++ public/JS/modules.js
@@ -0,0 +1,234 @@
+export const loadBooks = async (categoriesForBooksPage) => {
+	const gallerySection = document.querySelector("section.gallery");
+	const loadingAnimationEl = document.querySelector(".loading-animation");
+	for (let i = 0; i < categoriesForBooksPage.length; i++) {
+		const books = await (await fetch(`http://localhost:5000/books?category=${categoriesForBooksPage[i]}`)).json();
+		// each category has a wrapper
+		const categoryContainer = document.createElement("div");
+		categoryContainer.classList.add("category-container");
+
+		const booksWrapper = document.createElement("div");
+		booksWrapper.classList.add("books-wrapper");
+
+		gallerySection.append(categoryContainer);
+
+		const categoryHeader = document.createElement("h2");
+		categoryHeader.classList.add("category-header");
+		//
+		// when the data has been fetched, remove the loading animation
+		gallerySection.classList.remove("temp-gallery");
+		loadingAnimationEl.style.display = "none";
+		// the category name
+		categoryHeader.textContent = books.data[0].subject;
+		categoryContainer.append(categoryHeader);
+		books.data.forEach((book) => {
+			const bookCardDiv = document.createElement("div");
+			bookCardDiv.classList.add("book-card");
+
+			//
+			const bookUpper = document.createElement("div");
+			bookUpper.classList.add("book-upper");
+
+			const bookLower = document.createElement("div");
+			bookLower.classList.add("book-lower", "book-info");
+
+			//
+			const bookImg = document.createElement("img");
+			bookImg.src = book.links.img;
+
+			const bookImgLink = document.createElement("a");
+			bookImgLink.append(bookImg);
+			bookImgLink.href = `/book/${book._id}`;
+
+			const bookTitle = document.createElement("p");
+			bookTitle.textContent = book.name;
+
+			const bookTitleLink = document.createElement("a");
+			bookTitleLink.append(bookTitle);
+			bookTitleLink.href = `/book/${book._id}`;
+
+			bookUpper.append(bookImgLink);
+			bookUpper.append(bookTitleLink);
+
+			//
+			const bookBio = document.createElement("div");
+			bookBio.classList.add("book-bio");
+
+			const bookSubject = document.createElement("p");
+			bookSubject.textContent = book.subject;
+
+			const bookPublishingYear = document.createElement("p");
+			bookPublishingYear.textContent = book.publishing_year;
+
+			bookBio.append(bookSubject);
+			bookBio.append(bookPublishingYear);
+
+			const bookLinks = document.createElement("div");
+			bookLinks.classList.add("book-links");
+
+			const wikiLink = document.createElement("a");
+			if (book.links.wiki_link === "") {
+				wikiLink.classList.add("missing-wiki-source");
+				wikiLink.setAttribute("data-missing-book-id", book.name);
+
+				const missingBookInfo = document.createElement("p");
+				missingBookInfo.textContent = "لا يوجد صفحة لهذا الكتاب في ويكيبيديا على حسب عِلمنا";
+				missingBookInfo.classList.add("missing-wiki-source-notice", "wiki-notice-hide");
+				missingBookInfo.setAttribute("data-missing-book-id", book.name);
+
+				bookLower.append(missingBookInfo);
+			} else {
+				wikiLink.href = book.links.wiki_link;
+			}
+			wikiLink.textContent = "ويكيبيديا";
+
+			bookLinks.append(wikiLink);
+
+			bookLower.append(bookBio);
+			bookLower.append(bookLinks);
+
+			bookCardDiv.append(bookUpper);
+			bookCardDiv.append(bookLower);
+			booksWrapper.append(bookCardDiv);
+		});
+		categoryContainer.append(booksWrapper);
+	}
+	// calling this function here because it depends on `loadBooks` and `loadBooks` is async
+};
+
+export const popoversForMissinglinks = () => {
+	// the popovers for the missing links of the books
+	const wikiLinks = document.querySelectorAll(".missing-wiki-source");
+
+	wikiLinks.forEach((link) => {
+		link.addEventListener("click", () => {
+			const missigBookInfo = document.querySelector(`[data-missing-book-id=
+				'${link.dataset.missingBookId}']`);
+			missigBookInfo.classList.remove("wiki-visibility");
+			if (missigBookInfo.classList.contains("wiki-notice-hide")) {
+				missigBookInfo.classList.remove("wiki-notice-hide");
+				// then again, hide it after a short delay
+				setTimeout(() => {
+					missigBookInfo.classList.add("wiki-notice-hide");
+				}, 3500);
+				setTimeout(() => {
+					missigBookInfo.classList.add("wiki-visibility");
+				}, 4600);
+			}
+		});
+	});
+};
+
+export const searchFunction = () => {
+	const form = document.querySelector(".search");
+	const searchInput = document.querySelector("#search");
+	form.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const loadingAnimationEl = document.querySelector(".search-results-wrapper span");
+		loadingAnimationEl.classList.add("loading-animation");
+		const query = searchInput.value;
+		const result = await (await fetch(`/books?name=${query}`)).json();
+		if (result.success) {
+			displaySearchResults(result);
+			loadingAnimationEl.classList.remove("loading-animation");
+		} else {
+			displaySearchResults(false, query);
+			loadingAnimationEl.classList.remove("loading-animation");
+		}
+	});
+};
+
+export const displaySearchResults = (result, query) => {
+	if (!result) return noResults(query);
+	const searchResultsContainer = document.querySelector(".search-results");
+	searchResultsContainer.innerHTML = "";
+	searchResultsContainer.classList.add("search-results-loading");
+	const { data: book } = result;
+
+	const resultsEl = document.createElement("div");
+	resultsEl.classList.add("results-container");
+
+	const bookCardDiv = document.createElement("div");
+	bookCardDiv.classList.add("book-card");
+	// in order to highlight some books, from other page via a link, we need to give certain books special IDs.
+	//
+	const bookUpper = document.createElement("div");
+	bookUpper.classList.add("book-upper");
+
+	const bookLower = document.createElement("div");
+	bookLower.classList.add("book-lower", "book-info");
+
+	//
+	const bookImg = document.createElement("img");
+	bookImg.src = book.links.img;
+
+	const bookImgLink = document.createElement("a");
+	bookImgLink.append(bookImg);
+	bookImgLink.href = `/book/${book._id}`;
+
+	const bookTitle = document.createElement("p");
+	bookTitle.textContent = book.name;
+
+	const bookTitleLink = document.createElement("a");
+	bookTitleLink.append(bookTitle);
+	bookTitleLink.href = `/book/${book._id}`;
+
+	bookUpper.append(bookImgLink);
+	bookUpper.append(bookTitleLink);
+
+	//
+	const bookBio = document.createElement("div");
+	bookBio.classList.add("book-bio");
+
+	const bookSubject = document.createElement("p");
+	bookSubject.textContent = book.subject;
+
+	const bookPublishingYear = document.createElement("p");
+	bookPublishingYear.textContent = book.publishing_year;
+
+	bookBio.append(bookSubject);
+	bookBio.append(bookPublishingYear);
+
+	const bookLinks = document.createElement("div");
+	bookLinks.classList.add("book-links");
+
+	const wikiLink = document.createElement("a");
+	if (book.links.wiki_link === "") {
+		wikiLink.classList.add("missing-wiki-source");
+		wikiLink.setAttribute("data-missing-book-id", book.name);
+
+		const missingBookInfo = document.createElement("p");
+		missingBookInfo.textContent = "لا يوجد صفحة لهذا الكتاب في ويكيبيديا على حسب عِلمنا";
+		missingBookInfo.classList.add("missing-wiki-source-notice", "wiki-notice-hide");
+		missingBookInfo.setAttribute("data-missing-book-id", book.name);
+
+		bookLower.append(missingBookInfo);
+	} else {
+		wikiLink.href = book.links.wiki_link;
+	}
+	wikiLink.textContent = "ويكيبيديا";
+
+	bookLinks.append(wikiLink);
+
+	bookLower.append(bookBio);
+	bookLower.append(bookLinks);
+
+	bookCardDiv.append(bookUpper);
+	bookCardDiv.append(bookLower);
+	resultsEl.append(bookCardDiv);
+
+	searchResultsContainer.append(resultsEl);
+	const hr = document.createElement("hr");
+	searchResultsContainer.append(hr);
+};
+
+export const noResults = (query) => {
+	const searchResultsContainer = document.querySelector(".search-results");
+	const noResultsNoticeEl = document.createElement("h3");
+	noResultsNoticeEl.classList.add("no-search-results");
+	const arabicText = "لا نتائج بحث مطابقة ل";
+	noResultsNoticeEl.textContent = `${arabicText}"${query}"`;
+	searchResultsContainer.innerHTML = "";
+
+	searchResultsContainer.append(noResultsNoticeEl);
+};
--- public/JS/script_books.js
+++ public/JS/script_books.js
@@ -1,129 +1,25 @@
 // Copyright Yusuf Sabbag. Distributed under the MIT License respectively CC-BY NC License.
+import { loadBooks, searchFunction, popoversForMissinglinks, displaySearchResults, noResults } from "./modules.js";
 
-const loadBooks = async () => {
-	const categoriesForBooksPage = [97, 52, 3, 1, 67];
-	const gallerySection = document.querySelector("section.gallery");
-	const loadingAnimationEl = document.querySelector(".loading-animation");
-	for (let i = 0; i < categoriesForBooksPage.length; i++) {
-		const books = await (await fetch(`http://localhost:5000/books?category=${categoriesForBooksPage[i]}`)).json();
-		// each category has a wrapper
-		const categoryContainer = document.createElement("div");
-		categoryContainer.classList.add("category-container");
+const categoriesForBooksPage = [97, 52, 3, 1, 67];
 
-		const booksWrapper = document.createElement("div");
-		booksWrapper.classList.add("books-wrapper");
+// call popoveres
 
-		gallerySection.append(categoryContainer);
+searchFunction();
 
-		const categoryHeader = document.createElement("h2");
-		categoryHeader.classList.add("category-header");
-		//
-		// when the data has been fetched, remove the loading animation
-		gallerySection.classList.remove("temp-gallery");
-		loadingAnimationEl.style.display = "none";
-		// the category name
-		categoryHeader.textContent = books.data[0].subject;
-		categoryContainer.append(categoryHeader);
-		books.data.forEach((book) => {
-			const bookCardDiv = document.createElement("div");
-			bookCardDiv.classList.add("book-card");
-			// in order to highlight some books, from other page via a link, we need to give certain books special IDs.
-			//
-			const bookUpper = document.createElement("div");
-			bookUpper.classList.add("book-upper");
-
-			const bookLower = document.createElement("div");
-			bookLower.classList.add("book-lower", "book-info");
-
-			//
-			const bookImg = document.createElement("img");
-			bookImg.src = book.links.img;
-
-			const bookImgLink = document.createElement("a");
-			bookImgLink.append(bookImg);
-			bookImgLink.href = `/book/${book._id}`;
-
-			const bookTitle = document.createElement("p");
-			bookTitle.textContent = book.name;
-
-			const bookTitleLink = document.createElement("a");
-			bookTitleLink.append(bookTitle);
-			bookTitleLink.href = `/book/${book._id}`;
-
-			bookUpper.append(bookImgLink);
-			bookUpper.append(bookTitleLink);
-
-			//
-			const bookBio = document.createElement("div");
-			bookBio.classList.add("book-bio");
-
-			const bookSubject = document.createElement("p");
-			bookSubject.textContent = book.subject;
-
-			const bookPublishingYear = document.createElement("p");
-			bookPublishingYear.textContent = book.publishing_year;
-
-			bookBio.append(bookSubject);
-			bookBio.append(bookPublishingYear);
-
-			const bookLinks = document.createElement("div");
-			bookLinks.classList.add("book-links");
-
-			const wikiLink = document.createElement("a");
-			if (book.links.wiki_link === "") {
-				wikiLink.classList.add("missing-wiki-source");
-				wikiLink.setAttribute("data-missing-book-id", book.name);
-
-				const missingBookInfo = document.createElement("p");
-				missingBookInfo.textContent = "لا يوجد صفحة لهذا الكتاب في ويكيبيديا على حسب عِلمنا";
-				missingBookInfo.classList.add("missing-wiki-source-notice", "wiki-notice-hide");
-				missingBookInfo.setAttribute("data-missing-book-id", book.name);
-
-				bookLower.append(missingBookInfo);
-			} else {
-				wikiLink.href = book.links.wiki_link;
-			}
-			wikiLink.textContent = "ويكيبيديا";
-
-			bookLinks.append(wikiLink);
-
-			bookLower.append(bookBio);
-			bookLower.append(bookLinks);
-
-			bookCardDiv.append(bookUpper);
-			bookCardDiv.append(bookLower);
-			booksWrapper.append(bookCardDiv);
-		});
-		categoryContainer.append(booksWrapper);
-	}
-	// calling this function here because it depends on `loadBooks` and `loadBooks` is async
+async function main() {
+	await loadBooks(categoriesForBooksPage);
 	popoversForMissinglinks();
-};
-loadBooks();
+}
+main();
+const hideSearchResults = document.querySelector("#hide-search-results");
 
-const popoversForMissinglinks = () => {
-	// the popovers for the missing links of the books
-	const wikiLinks = document.querySelectorAll(".missing-wiki-source");
-
-	wikiLinks.forEach((link) => {
-		link.addEventListener("click", () => {
-			const missigBookInfo = document.querySelector(`[data-missing-book-id=
-				'${link.dataset.missingBookId}']`);
-			missigBookInfo.classList.remove("wiki-visibility");
-			if (missigBookInfo.classList.contains("wiki-notice-hide")) {
-				missigBookInfo.classList.remove("wiki-notice-hide");
-				// then again, hide it after a short delay
-				setTimeout(() => {
-					missigBookInfo.classList.add("wiki-notice-hide");
-				}, 3500);
-				setTimeout(() => {
-					missigBookInfo.classList.add("wiki-visibility");
-				}, 4600);
-			}
-		});
-	});
-};
-
+hideSearchResults.addEventListener("click", () => {
+	const searchResultsContainer = document.querySelector(".search-results");
+	const searchInput = document.querySelector("#search");
+	searchResultsContainer.innerHTML = "";
+	searchInput.value = "";
+});
 // the footer links
 const lgLink = document.querySelector(".language"); // the language link
 const lgInfo = document.querySelector(".language-description"); // the link's info
